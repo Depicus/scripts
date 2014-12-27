f531ebdf66846a8722ff49887edc9d015b0e483e
depicus <Depicus@users.noreply.github.com>
2014-12-27 11:09:00 +0000

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -2,3 +2,9 @@
 
 # first we need to add to the source list 
 
+echo 'deb http://download.webmin.com/download/repository sarge contrib' | sudo tee /etc/apt/sources.list.d/webmain.list
+echo 'deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib' | sudo tee /etc/apt/sources.list.d/webmain.list
+
+wget http://www.webmin.com/jcameron-key.asc && sudo apt-key add jcameron-key.asc
+
+sudo apt-get update && sudo apt-get install webmin -y
