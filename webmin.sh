30a88f0e7894fb8b2988152c96f9865a81b67c16
depicus <Depicus@users.noreply.github.com>
2020-04-30 18:10:46 +0100

    Updated to work on 20.04

--- webmin.sh
+++ webmin.sh
@@ -5,8 +5,9 @@
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 
 echo 'deb http://download.webmin.com/download/repository sarge contrib' | sudo tee /etc/apt/sources.list.d/webmain.list
-# echo 'deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib' | sudo tee /etc/apt/sources.list.d/webmain.list
 
 wget http://www.webmin.com/jcameron-key.asc && sudo apt-key add jcameron-key.asc
 
-sudo apt-get update && sudo apt-get install webmin -y
+sudo apt update
+
+sudo apt install webmin -y
