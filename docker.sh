508a6fd3dac62cb03b0a41a94570d61c72f68aeb
depicus <Depicus@users.noreply.github.com>
2020-08-21 16:24:36 +0100

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
