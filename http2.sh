c63aeacaa7d9845344db8c21f3754b32b08a742e
depicus <Depicus@users.noreply.github.com>
2021-01-02 22:02:58 +0000

    Create http2.sh

---
+++ http2.sh
@@ -0,0 +1,10 @@
+sudo a2enmod http2
+sudo apachectl stop
+sudo apt install php-fpm
+sudo a2enmod proxy_fcgi setenvif
+sudo a2enconf php7.4-fpm
+sudo a2dismod php7.4
+sudo a2dismod mpm_prefork
+sudo a2enmod mpm_event
+sudo apt install libapache2-mod-php
+sudo systemctl restart apache2
