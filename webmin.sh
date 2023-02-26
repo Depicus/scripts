762cfc2c141b50ebf8bb925c7aee01364ddecfad
depicus <Depicus@users.noreply.github.com>
2023-02-26 21:06:25 +0000

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -6,7 +6,8 @@ sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)
 
 echo 'deb http://download.webmin.com/download/repository sarge contrib' | sudo tee /etc/apt/sources.list.d/webmain.list
 
-wget http://www.webmin.com/jcameron-key.asc && sudo apt-key add jcameron-key.asc
+#wget http://www.webmin.com/jcameron-key.asc && sudo apt-key add jcameron-key.asc
+curl -fsSL https://download.webmin.com/jcameron-key.asc | sudo gpg --dearmor -o /usr/share/keyrings/webmin.gpg
 
 sudo apt update
 
